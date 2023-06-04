55396afe876927f5094558c00ae067f663851e65
Mariia Chernovolos <mariachernovolos@gmail.com>
2023-06-04 21:36:29 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,84 @@
+(() => {
+    "use strict";
+
+    const storedTheme = localStorage.getItem("theme");
+
+    const getPreferredTheme = () => {
+        if (storedTheme) {
+            return storedTheme;
+        }
+
+        return window.matchMedia("(prefers-color-scheme: dark)").matches
+            ? "dark"
+            : "light";
+    };
+
+    const setTheme = function (theme) {
+        if (theme === "auto" && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+            document.documentElement.setAttribute("data-bs-theme", "dark");
+        } else {
+            document.documentElement.setAttribute("data-bs-theme", theme);
+        }
+    };
+
+    setTheme(getPreferredTheme());
+
+    const showActiveTheme = (theme) => {
+        const activeThemeIcon = document.querySelector(".theme-icon-active use");
+        const btnToActive = document.querySelector(
+            `[data-bs-theme-value="${theme}"]`
+        );
+        const svgOfActiveBtn = btnToActive
+            .querySelector("svg use")
+            .getAttribute("href");
+
+        document.querySelectorAll("[data-bs-theme-value]").forEach((element) => {
+            element.classList.remove("active");
+        });
+
+        btnToActive.classList.add("active");
+        activeThemeIcon.setAttribute("href", svgOfActiveBtn);
+    };
+
+    window
+        .matchMedia("(prefers-color-scheme: dark)")
+        .addEventListener("change", () => {
+            if (storedTheme !== "light" || storedTheme !== "dark") {
+                setTheme(getPreferredTheme());
+            }
+        });
+
+    window.addEventListener("DOMContentLoaded", () => {
+        showActiveTheme(getPreferredTheme());
+
+        document.querySelectorAll("[data-bs-theme-value]").forEach((toggle) => {
+            toggle.addEventListener("click", () => {
+                const theme = toggle.getAttribute("data-bs-theme-value");
+                localStorage.setItem("theme", theme);
+                setTheme(theme);
+                showActiveTheme(theme);
+            });
+        });
+    });
+})();
+
+(() => {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+    })
+})()
+
+document.getElementById("myEmail").defaultValue = "mariia@mmmmms.com";
